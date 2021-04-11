d3fb21598882c3f1710822de753f30f60a7a091a
Aman Agnihotri <73701501+1AmanAgnihotri@users.noreply.github.com>
2021-04-11 17:14:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -626,7 +626,7 @@ help, so please call us to discuss your needs.</p>
 <footer class="last">
    <i class="fab fa-instagram" style="font-size: 25px;transform: translate(2100%,100%);"><a href="https://www.instagram.com/"></a></i>
     <i class="fab fa-twitter" style="font-size: 25px;transform: translate(3000%,100%);"><a href="https://www.twitter.com/"></a></i>
-    <i class="fab fa-facebook-f"  style="font-size: 25px;transform: translate(5400%,100%);"><a href="https://www.facebook.com/"></a></i>
+    <i class="fab fa-facebook-f"  style="font-size: 25px;transform: translate(1500%,100%);"><a href="https://www.facebook.com/"></a></i>
     <i class="far fa-copyright" style="font-size: 15px;transform: translate(-2400%,150%);"></i>
     
     <a href="/">All Rights Reserved</a>
